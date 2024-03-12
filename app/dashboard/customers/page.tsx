a61857fc791e9797d910bfc5e3dc86d275612095
Vatsal Dave <vatsaldave2002@gmail.com>
2024-03-12 15:58:16 +0530

    final changes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,27 @@
-const Page = () => {
-  return <p>Customers page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+const Page = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 };
 
 export default Page;
